ef6b6db27f9e015a054cbfb0f88daa1d4bb20a41
Kasdf Android <katie.login@runbox.com>
2018-05-06 16:26:51 +0300

    Initial commit

---
+++ dist/func.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+  $('.btn').click(function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+
+      if( $(this).hasClass('active') ) {
+        $(this).children('input').prop('checked',false);
+        $(this).removeClass('active');
+      } else {
+        $(this).children('input').prop('checked',true);
+        $(this).addClass('active');
+      }
+
+      parseCheckboxesForContent();
+  });
+
+  function parseCheckboxesForContent() {
+    $('input').each(function() {
+      var contentelement = $(this).attr('id').replace('selection','content');
+      if( $(this).is(':checked') ) {
+          $('#' + contentelement ).show();
+      } else {
+          $('#' + contentelement ).hide();
+      }
+    });
+  }
+
+});
